3a5f4fe90ba5d3cb2eab847a77cd13ea6df08d7d
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-08-28 18:46:14 +0000

    Exemplo

--- meu-app/App.tsx
+++ meu-app/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import TelaDaListaDeTarefas from './components/TelaDaListaDeTarefas';
 
-export default function App() {
+ export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TelaDaListaDeTarefas />
       <StatusBar style="auto" />
     </View>
   );
---
+++ meu-app/components/TelaDaListaDeTarefas.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
+import { tarefasMockadas } from '../data/tarefas';
+import { Tarefa } from '../types/Tarefa';
+
+export default function TelaDaListaDeTarefas() {
+  const [tarefas, setTarefas] = useState<Tarefa[]>([]);
+  const [carregando, setCarregando] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTarefas(tarefasMockadas);
+      setCarregando(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Minhas Tarefas</Text>
+      {carregando ? (
+        <View style={styles.carregando}>
+          <Text>Carregando...</Text>
+          <ActivityIndicator size="large" color="#0000ff" style={styles.indicador}/>
+        </View>
+      ) : (
+        <FlatList
+          data={tarefas}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => <Text style={styles.item}>{item.id} - {item.titulo}</Text>}
+        />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20, marginTop: 50 },
+  title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
+  carregando: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  indicador: { marginTop: 12 },
+  item: { paddingVertical: 8, fontSize: 16 },
+});
